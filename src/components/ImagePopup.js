161dc268d9f1cd4cfb9bc73c7732b6b7530ee280
Omri Ruvio <omriruvio@gmail.com>
2022-04-13 18:25:12 +0300

    fix(preview): add state to prevent undef on close

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,10 +1,22 @@
+import React from 'react';
+
 export default function ImagePopup(props) {
+  const [popupImageLink, setPopupImageLink] = React.useState('');
+  const [popupImageName, setPopupImageName] = React.useState('');
+
+  React.useEffect(() => {
+    if (props.card) {
+      setPopupImageLink(props.card.link);
+      setPopupImageName(props.card.name);
+    }
+  }, [props.card]);
+
   return (
     <div className={`popup popup_type_preview ${props.card ? 'popup_active' : ''}`}>
       <div className="popup__window popup__window_type_preview">
         <button type="button" className="popup__close-button" aria-label="close" onClick={props.onClose}></button>
-        <img src={props.card?.link || ''} alt="preview" className="popup__preview-image" />
-        <p className="popup__description">{props.card?.name || ''}</p>
+        <img src={popupImageLink} alt="preview" className="popup__preview-image" />
+        <p className="popup__description">{popupImageName}</p>
       </div>
     </div>
   );
