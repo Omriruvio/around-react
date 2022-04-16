5cb2a41576989e6069a4768f046ebcde0e479a0e
Omri Ruvio <omriruvio@gmail.com>
2022-04-16 10:56:12 +0300

    feature(validation): add new card form validation

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -3,46 +3,80 @@ import React from 'react';
 
 export default function AddPlacePopup(props) {
   const { isOpen, onClose, onAddPlaceSubmit, buttonText, onPopupClick } = props;
-  const [cardName, setCardName] = React.useState('');
-  const [cardLink, setCardLink] = React.useState('');
+  // const [cardName, setCardName] = React.useState('');
+  // const [cardLink, setCardLink] = React.useState('');
+  const [inputs, setInputs] = React.useState({});
+  const [validation, setValidation] = React.useState({});
+  const [isValid, setIsValid] = React.useState(false);
+  // const [showError, setShowError] = React.useState(false);
 
   React.useEffect(() => {
-    setCardName('');
-    setCardLink('');
+    setInputs({});
+    if (!isOpen) setValidation({});
   }, [isOpen]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onAddPlaceSubmit({ name: cardName, link: cardLink });
+    onAddPlaceSubmit({ name: inputs.name, link: inputs.link });
   };
+
+  const handleInput = (event) => {
+    setInputs({
+      ...inputs,
+      [event.target.name]: event.target.value,
+    });
+    setValidation({
+      ...validation,
+      [event.target.name]: event.target.validationMessage,
+    });
+  };
+
+  React.useEffect(() => {
+    if (isOpen) {
+      const formIsValid = inputs.link && inputs.title && !Object.values(validation).some((val) => Boolean(val));
+      setIsValid(formIsValid || false);
+    }
+  }, [validation, inputs, isOpen]);
 
   return (
     <div onMouseDown={onPopupClick}>
-      <PopupWithForm onSubmit={handleSubmit} name="new-card" title="New place" isOpen={isOpen} onClose={onClose} buttonText={buttonText}>
+      <PopupWithForm
+        isValid={isValid}
+        onSubmit={handleSubmit}
+        name="new-card"
+        title="New place"
+        isOpen={isOpen}
+        onClose={onClose}
+        buttonText={buttonText}
+      >
         <input
-          onChange={(event) => setCardName(event.target.value)}
-          value={cardName}
+          onChange={handleInput}
+          value={inputs.title || ''}
           id="image-title-input"
           type="text"
           className="form__input form__input_type_image-title"
           placeholder="Title"
-          name="newCardFormImageTitleInput"
+          name="title"
           required
           minLength="1"
           maxLength="30"
         />
-        <span id="image-title-input-error" className="form__input-error"></span>
+        <span id="image-title-input-error" className={`form__input-error ${isValid ? '' : 'form__input-error_active'}`}>
+          {validation.title}
+        </span>
         <input
-          onChange={(event) => setCardLink(event.target.value)}
-          value={cardLink}
+          onChange={handleInput}
+          value={inputs.link || ''}
           id="image-link-input"
           type="url"
           className="form__input form__input_type_image-link"
           placeholder="Image link"
-          name="newCardFormImageLinkInput"
+          name="link"
           required
         />
-        <span id="image-link-input-error" className="form__input-error"></span>
+        <span id="image-link-input-error" className={`form__input-error ${isValid ? '' : 'form__input-error_active'}`}>
+          {validation.link}
+        </span>
       </PopupWithForm>
     </div>
   );
